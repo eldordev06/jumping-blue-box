84b5a47b09ef135ff9ac4aefe5bcb14d821c49b8
Eldor <eldorforprogramming@gmail.com>
2024-05-26 13:53:21 +0500

    mobile version swiping added

--- index.js
+++ index.js
@@ -44,3 +44,63 @@ function animateBox(htmlElem, htmlElemTwo) {
         htmlElemTwo.classList.remove("jumpShadow");
     });
 }
+
+let pageWidth = window.innerWidth || document.body.clientWidth;
+let treshold = Math.max(1, Math.floor(0.01 * (pageWidth)));
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
+const gestureZone = document.querySelector(".box-container");
+
+gestureZone.addEventListener('touchstart', function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture(event);
+}, false);
+
+function handleGesture() {
+    let x = touchendX - touchstartX;
+    let y = touchendY - touchstartY;
+    let xy = Math.abs(x / y);
+    let yx = Math.abs(y / x);
+    if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
+        if (yx <= limit) {
+            if (x < 0) {
+                if (axisX > 0) {
+                    axisX -= movingLength;
+                    box.style.left = axisX + "%";
+                    animateBox(box, jumpShadow);
+                }
+            } else {
+                if (axisX < 90) {
+                    axisX += movingLength;
+                    box.style.left = axisX + "%";
+                    animateBox(box, jumpShadow);
+                }
+            }
+        }
+        if (xy <= limit) {
+            if (y < 0) {
+                if (axisY > 0) {
+                    axisY -= movingLength;
+                    box.style.top = axisY + "%";
+                    animateBox(box, jumpShadow);
+                }
+            } else {
+                if (axisY < 90) {
+                    axisY += movingLength;
+                    box.style.top = axisY + "%";
+                    animateBox(box, jumpShadow);
+                }
+            }
+        }
+    }
+}
